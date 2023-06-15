be12eaa83c35caeec54163340b7721314f46fde0
KevinBatist <kevinbatistacardoso@gmail.com>
2023-06-14 22:19:41 -0300

    Fatiamento e integração com MySQL

--- canesgril/settings.py
+++ canesgril/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'canesgril.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# CONEXÃO COM O BANCO DE DADOS MYSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_churras',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'OPTIONS' : {
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -118,7 +126,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR / 'templates/static',
     
 ]
--- churras/views.py
+++ churras/views.py
@@ -4,5 +4,20 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    pratos = {
+        1: 'Picanha',
+        2: 'Maminha',
+        3: 'Fraldinha',
+        4: 'Bife ancho',
+
+
+    }
+
+    contexto = {
+        'lista_pratos' : pratos,
+
+    }
+
+
+    return render(request, 'index.html', contexto)
     # return HttpResponse('<h1>Churrasco - Canes Gril</h1><p>Toma o primeiro site<p>')
