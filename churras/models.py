f4a29fc062588e140918466e75efceeef3a13019
KevinBatist <kevinbatistacardoso@gmail.com>
2023-06-16 22:21:41 -0300

    Atualização da nova view

--- churras/models.py
+++ churras/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+#Esta classe de models se tornará uma tabela no banco de dados
+class Prato(models.Model):
+    #Daqui pra frente serao os campos da tabela
+    #ou seja, os atributos da classe
+    nome_prato = models.CharField(max_length = 100,verbose_name="Nome do prato",)
+
+    ingredientes = models.TextField(verbose_name='Ingredientes',)
+
+    modo_preparo = models.TextField(verbose_name='Modo de preparo',)
+
+    tempo_preparo = models.PositiveIntegerField(verbose_name='Tempo de preparo',)
+
+    rendimento = models.CharField(max_length=100,verbose_name='Rendimento',)
+
+    categoria = models.CharField(max_length=100, verbose_name='Categoria')
+
+    data_prato = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.nome_prato
+    class Meta:
+        verbose_name = 'Prato'
+        verbose_name_plural = 'Pratos'
+
--- churras/views.py
+++ churras/views.py
@@ -1,23 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Prato
 # Create your views here.
 
 def index(request):
-    pratos = {
-        1: 'Picanha',
-        2: 'Maminha',
-        3: 'Fraldinha',
-        4: 'Bife ancho',
-
-
-    }
-
-    contexto = {
-        'lista_pratos' : pratos,
-
-    }
-
-
+    pratos = Prato.objects.all()
+    contexto = {'lista_pratos' : pratos,}
     return render(request, 'index.html', contexto)
     # return HttpResponse('<h1>Churrasco - Canes Gril</h1><p>Toma o primeiro site<p>')
+
+def churrasco(request, prato_id):
+    # prato = Prato.objects.filter(pk = prato_id)
+    prato = get_object_or_404(Prato, pk = prato_id)
+    contexto = {'prato' : prato}
+    return render(request, 'churrasco.html', contexto)
