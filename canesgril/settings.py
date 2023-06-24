20ce55594f529c637f89e4e0f529ebdf8b3706c0
KevinBatist <kevinbatistacardoso@gmail.com>
2023-06-23 22:21:16 -0300

    Inicio do app usuarios

--- canesgril/settings.py
+++ canesgril/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios',
     'pessoas',
     'churras',
     'django.contrib.admin',
---
+++ usuarios/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def dashboard(request):
+    pass
+def logout(request):
+    pass
